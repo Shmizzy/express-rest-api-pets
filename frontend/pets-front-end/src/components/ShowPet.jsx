c7541fc1c2a000547b89eb63f123a75f1ba9dd56
“Karlon <karlon.heileman@gmail.com>
2024-05-21 15:59:57 -0500

    ShowPet

--- frontend/pets-front-end/src/components/ShowPet.jsx
+++ frontend/pets-front-end/src/components/ShowPet.jsx
@@ -1,48 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { fetchPets } from '../services/petService';
+import PropTypes from "prop-types";
+import './petCard.css';
 
-function PlanetsList() {
-    const [planets, setPlanets] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-  
-    useEffect(() => {
-      const getPets = async () => {
-        try {
-          const petsData = await fetchPets();
-          setPlanets(planetsData);
-          setLoading(false);
-        } catch (error) {
-          setError('Failed to fetch planets. Please try again later.');
-          setLoading(false);
-        }
-      };
-      getPets();
-    }, []);
-  
-    return (
-      <div>
-        <h2>Pets</h2>
-        {loading ? (
-          <div className="loading">Loading...</div>
-        ) : error ? (
-          <div className="error">{error}</div>
-        ) : (
-          <ul>
-            {planets.map((planet) => (
-              <li key={planet.name}>
-                <div>
-                  <h3>{pet.name}</h3>
-                  <p>Name: {pet.name}</p>
-                  <p>Age: {pet.age}</p>
-                  <p>Breed: {pet.breed}</p>
+const PetCard = ({ pet }) => {
+  return (
+    <div className="petCard">
+      <h1>{pet.name}</h1>
+      <h2>Age: {pet.age}</h2>
+      <h2>Breed: {pet.breed}</h2>
+    </div>
+  );
+};
 
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
-    );
-  }
-  
-  export default PetsList;
+PetCard.propTypes = {
+  pet: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    breed: PropTypes.string.isRequired,
+    age: PropTypes.number.isRequired
+  }).isRequired
+};
+
+export default PetCard;
